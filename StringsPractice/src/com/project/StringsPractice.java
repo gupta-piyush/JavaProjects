696ab78fd11c15812c4ca4cee26f32a70739320f
Piyush Gupta <guptapiyush86@live.com>
2013-12-22 18:26:11 -0500

    final commit. code refactoring done.

--- StringsPractice/src/com/project/StringsPractice.java
+++ StringsPractice/src/com/project/StringsPractice.java
@@ -1,56 +1,54 @@
 package com.project;
 
 public class StringsPractice {
-	static 
-	{
-		System.out.print("Welcome to UF!! ");
+	// To print a statement before main!
+	static {
+		System.out.println("Welcome to UF!! ");
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		System.out.println("Hello World");
-		//String a = "abcbbdfhbb";
-		//String b = "bbe";
+		// String a = "abcbbdfhbb";
+		// String b = "bbe";
 		String a = "abcabcdababcdabcdabde";
 		String b = "abcdabd";
-		System.out.println("Result: "+isSubstring(a,b));
+		System.out.println("Result: " + isSubstring(a, b));
 	}
-	
-	static boolean isSubstring(String a, String b){
-		if(b.length()>a.length())
+
+	static boolean isSubstring(String a, String b) {
+		if (b.length() > a.length())
 			return false;
-		//Adding a new line to test for commits.
-		//new line
 		char[] newa = a.toCharArray();
 		char[] newb = b.toCharArray();
-		int index=0, nextIndex=0, count = 0,j=0;
-		while(index<a.length()){
-			//for(int j=0;j<b.length();j++){
-				System.out.println("Characters are: "+newa[index]+" & "+ newb[j] );
-				if((newa[index]==newb[0]) && j!=0){
-					if(count<1){
-						count++;
-						nextIndex=index;
-					}
+		int index = 0, nextIndex = 0, count = 0, j = 0;
+		while (index < a.length()) {
+			System.out.println("Characters are: " + newa[index] + " & "
+					+ newb[j]);
+			if ((newa[index] == newb[0]) && j != 0) {
+				if (count < 1) {
+					count++;
+					nextIndex = index;
 				}
-				if(newa[index]!=newb[j]){
-					index++;
-					if(count<1)
-						nextIndex++;
-					//break;
-					j=0;
-					index=nextIndex;
-					count=0;
-				}
-				else{
-					if(count<1)
-						nextIndex++;
-					index++;
-					j++;
-					if(j==(b.length()))
-						return true;
-				}
-				
-			//}
+			}
+			//No match: reset the values
+			if (newa[index] != newb[j]) {
+				index++;
+				if (count < 1)
+					nextIndex++;
+				// resetting to start from next possible match
+				j = 0;
+				index = nextIndex;
+				count = 0;
+			} 
+			// Match of character. Move to next
+			else {	
+				if (count < 1)
+					nextIndex++;
+				index++;
+				j++;
+				if (j == (b.length()))
+					return true;
+			}
 		}
 		return false;
 	}
